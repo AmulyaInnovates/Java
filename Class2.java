4f3ec2f74542fc36576a897e5cd49a906ae894d9
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2024-11-03 20:53:38 +0530

    Initial Commit

---
+++ Class2.java
@@ -0,0 +1,9 @@
+public class Class2{
+    public static void main(String[] args){
+        System.out.println("Hey Hi Java, this is Amulya, this is my 1st Class learning about Java!");
+        System.out.println(956066110);
+        System.out.println("My Roll no. is: "+ 05);
+        System.out.println(1 + 2 + 3 + 4);
+        System.out.println("My mobile number and"+" My Roll Number are "+ 956066110 +" & 05");
+    }
+}
