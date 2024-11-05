e250d48714cce47e0df489d72171d75b8ddb8f1b
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2024-11-05 21:13:24 +0530

    Initial Commit

---
+++ Project2.java
@@ -0,0 +1,12 @@
+public class Project2 {
+    public static void main(String[] args){
+        int Febdays= 28;
+        if (Febdays == 29) {
+            System.out.println("It is a Leap Year");   
+        }
+        else{
+            System.out.println("It is not a Leap Year");
+        }
+    } 
+    
+}
---
+++ Project2_2.java
@@ -0,0 +1,12 @@
+public class Project2_2 {
+    public static void main(String[] args){
+        int Yearnumber= 2020;
+        if (Yearnumber % 4 == 0){
+            System.out.println("It is a Leap Year");
+        }
+        else{
+            System.out.println("It is not a Leap Year");
+        }
+    }
+    
+}
