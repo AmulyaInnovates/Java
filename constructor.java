27991eb4d09385fe96daa1dce4d15aaba7612f2a
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2024-12-26 18:47:18 +0530

    Initial Commit

---
+++ constructor.java
@@ -0,0 +1,14 @@
+class Student extends Person{
+    private String major;
+
+    public Student(String major, String name, int age){
+        super(name,age);
+        this.major=major;}
+
+    public String get_data(){
+        return major;
+    }
+
+}
+
+//-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
