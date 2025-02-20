d9af8d951f8cab7367556de814d17f05b7306771
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2025-02-20 20:47:08 +0530

    Intitial Commit

---
+++ Tower_Of_Haloi.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Tower_Of_Haloi {
+    public static void toh(int n,int t1,int t2,int t3){
+        if (n>0){
+        toh(n-1,t1,t3,t2);
+        System.out.println("The Disk number "+n+" will be moved from "+t1+" to "+t3);
+        toh(n-1,t2,t1,t3);
+    }
+}
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Enter the value of disks for the Tower of Haloi");
+        int n= sc.nextInt();
+        
+        System.out.println("Enter the value for Tower 1");
+        int t1= sc.nextInt();
+        System.out.println("Enter the value for Tower 1");
+        int t2= sc.nextInt();
+        System.out.println("Enter the value for Tower 1");
+        int t3= sc.nextInt();
+
+        toh(n,t1,t2,t3);
+        
+    }
+}
