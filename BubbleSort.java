b202ea8d55e1e1d75b645bea2d4dc1d5b9fe2f6c
Amulya958 <amulya.gupta.2556@gdgoenkarohini.edu.in>
2025-03-21 18:41:39 +0530

    Initial Commit

---
+++ BubbleSort.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class BubbleSort {
+
+    public static void bubblesort(int[] arr, int n){
+        int i,j,temp;
+        boolean swapped;
+
+        for(i=0;i<n-1;i++){
+            swapped=false;
+            for(j=0;j<n-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    temp= arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+
+        }
+    }
+
+    static void printthenumber(int[] arr, int n1){
+        for(int i=0; i<n1;i++){
+            System.out.print(arr[i] + " ");
+        System.out.println();
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Please enter the number of values in the array.");
+        int n= sc.nextInt();
+
+        int[] arr= new int[n];
+
+        System.out.println("Now one-by-one give the values for each and every index.");
+        for(int i=0;i<n;i++){
+            System.out.println("Enter the value at index number :- " + i);
+            arr[i]= sc.nextInt();
+        }
+
+        bubblesort(arr, n);
+        System.out.println("Sorted Array :- ");
+        printthenumber(arr, n);
+    }
+}
